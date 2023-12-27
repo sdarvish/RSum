5017bf94d502a5106c6b425ea6699e81cd4be4dc
Saeed Darvish <sd.saeed.darvish@gmail.com>
2023-12-27 12:51:25 +0330

    🌱Initialize project🚀

---
+++ common/src/androidMain/kotlin/ir/rsum/Platform.android.kt
@@ -0,0 +1,7 @@
+package ir.rsum
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
